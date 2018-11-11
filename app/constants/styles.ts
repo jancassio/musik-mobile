fbc4d7420dbc62463148695641990f95acdad003
Jan Cássio <hey@jancassio.com>
2018-11-10 21:39:58 -0500

    created global styles file and added default colors

---
+++ app/constants/styles.ts
@@ -0,0 +1,8 @@
+export enum Colors {
+  Primary = '#ffffff',
+  Secondary = '#240f39',
+  BrandPrimary = '#fe5266',
+  BrandSecondary = '#fd7F54',
+  Disabled = '#e9e9e9',
+  DarkGrey = '#707070'
+}
