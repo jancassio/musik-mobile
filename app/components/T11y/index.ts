3480e099f93dc56a22b1066b20dc469d22c87535
Jan Cássio <hey@jancassio.com>
2018-11-12 23:42:22 -0500

    exposing more t11y components

--- app/components/T11y/index.ts
+++ app/components/T11y/index.ts
@@ -1,7 +1,23 @@
+import Body from './Body'
 import Headline from './Headline'
+import Label from './Label'
+import LargeBody from './LargeBody'
+import PlaceHolder from './PlaceHolder'
+import Quote from './Quote'
+import SecondaryBody from './SecondaryBody'
+import Subheader from './Subheader'
+import Title from './Title'
 
 const T11y = {
+  Body,
   Headline,
+  Label,
+  LargeBody,
+  PlaceHolder,
+  Quote,
+  SecondaryBody,
+  Subheader,
+  Title,
 }
 
 export default T11y;
